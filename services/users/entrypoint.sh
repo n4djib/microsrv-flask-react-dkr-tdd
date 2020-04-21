40ae29e4fb19c4f97de9fa62c9ca5d3468968028
n4djib <n4djib@gmail.com>
2020-04-21 18:13:58 +0100

    finished Postgres, with some problems

--- services/users/entrypoint.sh
+++ services/users/entrypoint.sh
@@ -1,19 +1,12 @@
 #!/bin/sh
 
-echo " "
-echo " "
 echo "Waiting for postgres..."
-echo " "
-echo " "
 
-while ! nc -z users-db 5432; do
-    sleep 0.1
-done
+# while ! nc -z users-db 5432; do
+#     echo "--- [nc -z users-db 5432] ---"
+#     sleep 0.1
+# done
 
-echo " "
-echo " "
 echo "PostgreSQL started"
-echo " "
-echo " "
 
 python manage.py run -h 0.0.0.0
