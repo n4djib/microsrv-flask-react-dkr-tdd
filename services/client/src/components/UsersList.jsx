a8999c3aa9e9018e2eef9e87905596b378cef759
n4djib <n4djib@gmail.com>
2020-05-03 23:41:42 +0100

    end of React Bootsrap section

--- services/client/src/components/UsersList.jsx
+++ services/client/src/components/UsersList.jsx
@@ -7,7 +7,7 @@ const UsersList = (props) => {
             props.users.map((user) => {
                 return (
                     <h4 key={user.id}
-                        className="card card-body bg-light"
+                        className="well"
                         >{user.username}
                     </h4>
                 )
